ed71379dd1ee0de09e2fd916e10f9c82ffe81496
Abhinav <AbhinavSunil@hotmail.com>
2026-05-01 23:11:44 +0530

    Feat : added extra react port

--- src/socket.js
+++ src/socket.js
@@ -11,7 +11,7 @@ const cache = new Redis();
 
 const io = new Server(httpServer, {
   cors: {
-    origin: 'http://localhost:5173', // Vite default port
+    origin: ['http://localhost:5173', 'http://localhost:5174'], // Vite default and secondary ports
     methods: ['GET', 'POST'],
   },
 });
